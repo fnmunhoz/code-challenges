41618a9be740fa70d50637502337f0db088b14d4
Felipe Munhoz <felipe@felipemunhoz.com>
2020-01-24 17:54:53 -0300

    Simplify implementation a bit

--- binary-search-tree/js/bst.js
+++ binary-search-tree/js/bst.js
@@ -8,10 +8,7 @@ function searchBST(node, value) {
   }
 
   if (node.value > value) {
-    if (node.left) {
-      return searchBST(node.left, value);
-    }
-    return value;
+    return searchBST(node.left, value);
   }
 
   return searchBST(node.right, value);
